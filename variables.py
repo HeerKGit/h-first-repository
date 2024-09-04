384b486f5a3b1dae610e681400e447a5262369a4
Heer <hrk7077@g.rit.edu>
2024-09-04 15:49:09 +0400

    version 1

---
+++ variables.py
@@ -0,0 +1,14 @@
+
+
+def variables_pratice():
+    age = 216
+    days = 365
+    pet_name = 'goldie'
+    pi = 3.1415
+
+    print('age = ', age)
+    print('days= ', days)
+    print ('pet_name = '  ,pet_name)
+    print ('pi = ', pi)
+
+variables_pratice()
